dbc6fdfaed1387a1af737e222ea54368106b0c6b
wyt <168687208+wytlines100@users.noreply.github.com>
2024-12-04 10:01:23 -0800

    Update injection.js

--- injection.js
+++ injection.js
@@ -416,6 +416,8 @@ function modifyCode(text) {
 					const module = args.length > 1 && getModule(args[1]);
 					if (module) {
 						module.toggle();
+						game$1.chat.addChat({
+							
 						});
 					}
 					else if (args[1] == "all") {
